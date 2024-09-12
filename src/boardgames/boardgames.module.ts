b5fc17b12cdb24b31c0344356303df1e87eed76b
Wellesley Mussolini <iei.muss@gmail.com>
2024-09-11 23:46:40 -0300

    feat: add Boardgames module and schema
    - Created BoardgamesModule to encapsulate boardgames-related functionality.
    - Defined Boardgame schema to represent boardgame data structure in the database.
    - Integrated schema with the module for managing boardgame data and operations.

---
+++ src/boardgames/boardgames.module.ts
@@ -0,0 +1,17 @@
+import { Module } from "@nestjs/common";
+import { MongooseModule } from "@nestjs/mongoose";
+import { BoardGame, BoardGameSchema } from "src/schemas/boardgames.schema";
+import { BoardGamesService } from "./boardgames.service";
+import { BoardGamesController } from "./boardgames.controller";
+
+@Module({
+    imports: [
+        MongooseModule.forFeature([{
+            name: BoardGame.name,
+            schema: BoardGameSchema,
+        }])
+    ],
+    providers: [BoardGamesService],
+    controllers: [BoardGamesController],
+})
+export class BoardGamesModule { }
---
+++ src/schemas/boardgames.schema.ts
@@ -0,0 +1,33 @@
+import { Schema, Prop, SchemaFactory } from "@nestjs/mongoose";
+
+@Schema()
+export class BoardGame {
+    @Prop({ required: true })
+    name: string;
+
+    @Prop({ required: true })
+    image: string;
+
+    @Prop({ required: true })
+    status: string;
+
+    @Prop({ required: true })
+    price: string;
+
+    @Prop({ required: true })
+    ageToPlay: string;
+
+    @Prop({ required: true })
+    description: string;
+
+    @Prop({ required: true })
+    playTime: string;
+
+    @Prop({ required: true })
+    minimumPlayersToPlay: string;
+
+    @Prop({ required: true })
+    maximumPlayersToPlay: string;
+};
+
+export const BoardGameSchema = SchemaFactory.createForClass(BoardGame);
