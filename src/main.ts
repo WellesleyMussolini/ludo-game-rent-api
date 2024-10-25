0be170b4fd6e4cba87b7bf35d176019eea43fe5e
Wellesley Mussolini <iei.muss@gmail.com>
2024-10-25 09:26:00 -0300

    chore: set chores path route

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from 'src/app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['https://ludo-games-rent-api.vercel.app/'],
+    origin: ['https://ludo-game-rent-api.onrender.com'],
   });
   await app.listen(process.env.PORT || 3001);
 }
