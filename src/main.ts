3d5cc0b8fae54d8688d4285c6d2c2e38301c686b
Wellesley Mussolini <iei.muss@gmail.com>
2024-10-24 21:44:26 -0300

    feat: vercel config to deploy api

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,9 @@ import { AppModule } from 'src/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: ['https://ludo-games-rent-api.vercel.app/'],
+  });
   await app.listen(process.env.PORT || 3001);
 }
 bootstrap();
