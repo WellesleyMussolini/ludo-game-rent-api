6bd6da5e680805a2fad370d0ba0569cc447a02ad
Wellesley Mussolini <iei.muss@gmail.com>
2024-10-25 09:48:47 -0300

    chore: enable cors

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,7 @@ import { AppModule } from 'src/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: ['https://ludo-game-rent-api.onrender.com'],
-  });
+  app.enableCors();
   await app.listen(process.env.PORT || 3001);
 }
 bootstrap();
